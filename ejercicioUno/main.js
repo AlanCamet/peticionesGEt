640e45b014509ce6a328ce8dbf0d734c8b184001
Camet <alan_israel_97@hotmail.com>
2023-06-22 19:57:54 -0600

    docs: Primer commit

---
+++ ejercicioUno/main.js
@@ -0,0 +1,22 @@
+let pokemonesARray = []
+
+async function getApi(){
+     let request = await fetch("https://pokeapi.co/api/v2/pokemon/")
+     let response = await request.json()
+     let data = response.results
+    for (let i = 0; i<= data.length; i++){
+
+        let request = await fetch (data[i].url)
+        let response = await request.json()
+        let dataDos = response.types[0]
+        console.log("El nombre del pokemon es " + data[i].name + " y su tipo es " + dataDos.type.name)
+        
+    }
+    
+}
+
+function consolePokemon(request){
+    console.log(request.types.type)
+}
+
+getApi()
