80b53733d2ee65e60b88249113866dc513d02ccf
Camet <alan_israel_97@hotmail.com>
2023-06-22 21:03:42 -0600

    fix: se corrigio error de puesto 100 no existia en el length

--- ejercicioDos/main.js
+++ ejercicioDos/main.js
@@ -2,7 +2,7 @@ async function peticionLibros(){
     let request = await fetch("http://openlibrary.org/search.json?q=i+robot)")
     let response = await request.json()
     let data = response.docs
-    for(let i=0; i <= data.length; i++){
+    for(let i=0; i < data.length; i++){
         
         console.log(i)
             console.log("El nombre del libro es: "+ data[i].title + " y su autor es: " +  `${data[i].author_name ? data[i].author_name[0] : 'NO DATA'}`)
