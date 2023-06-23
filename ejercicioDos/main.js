e2119949f3f7e77c2b24a82f50034f8579a3207f
Camet <alan_israel_97@hotmail.com>
2023-06-22 20:40:52 -0600

    feat: se añade ejercicio dos

---
+++ ejercicioDos/main.js
@@ -0,0 +1,13 @@
+async function peticionLibros(){
+    let request = await fetch("http://openlibrary.org/search.json?q=i+robot)")
+    let response = await request.json()
+    let data = response.docs
+    for(let i=0; i <= data.length; i++){
+        
+        console.log("hi " + i)
+           await console.log("El nombre del libro es: "+ data[i].title + " y su autor es: " + data[i].author_name[0])
+        
+    }
+}
+
+peticionLibros()
